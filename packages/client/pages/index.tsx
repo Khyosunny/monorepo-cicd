7de8688b938b72452a342de19512034f524a7c84
Khyosunny <kimhyo_0218@naver.com>
2022-10-16 14:27:51 +0900

    fix: text

--- packages/client/pages/index.tsx
+++ packages/client/pages/index.tsx
@@ -1,11 +1,12 @@
 import Button from '@/components/Button';
 
 function HomePage() {
+  console.log('home');
   return (
     <main>
       <h1>Hi</h1>
       <Button>Github Action !</Button>
-      <p>test</p>
+      <p>test~~~~</p>
     </main>
   );
 }
